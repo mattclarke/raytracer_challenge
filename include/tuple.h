497b9fb08673a2fcb870c77cac0f65cafb64959f
Matt Clarke <matt.clarke@ess.eu>
2023-02-19 15:44:19 +0100

    pulled tuple code out into a separate file

---
+++ include/tuple.h
@@ -0,0 +1,55 @@
+#include <cmath>
+
+struct Tuple {
+    double x;
+    double y;
+    double z;
+    double w;
+
+    bool is_point() {
+        return w == 1.0;
+    }
+
+    bool is_vector() {
+        return w == 0.0;
+    }
+
+};
+
+bool operator==(const Tuple &lhs, const Tuple &rhs) {
+    // TODO: use epsilon for comparison?
+    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w;
+}
+
+Tuple operator+(const Tuple &lhs, const Tuple &rhs) {
+    return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w};
+}
+
+Tuple operator-(const Tuple &lhs, const Tuple &rhs) {
+    return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w};
+}
+
+Tuple operator-(const Tuple &rhs) {
+    return {-rhs.x, -rhs.y, -rhs.z, -rhs.w};
+}
+
+Tuple operator*(const Tuple &tuple, double scalar) {
+    return {tuple.x * scalar, tuple.y * scalar, tuple.z * scalar, tuple.w * scalar};
+}
+
+Tuple operator/(const Tuple &tuple, double scalar) {
+    return {tuple.x / scalar, tuple.y / scalar, tuple.z / scalar, tuple.w / scalar};
+}
+
+Tuple point(double x, double y, double z) {
+    return {x, y, z, 1.0};
+}
+
+Tuple vector(double x, double y, double z) {
+    return {x, y, z, 0.0};
+}
+
+double magnitude(const Tuple &tuple) {
+    return std::sqrt(tuple.x * tuple.x + tuple.y * tuple.y + tuple.z * tuple.z + tuple.w * tuple.w);
+}
+
--- tests/tuple_test.cpp
+++ tests/tuple_test.cpp
@@ -1,59 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <cmath>
-
-struct Tuple {
-    double x;
-    double y;
-    double z;
-    double w;
-
-    bool is_point() {
-        return w == 1.0;
-    }
-
-    bool is_vector() {
-        return w == 0.0;
-    }
-
-};
-
-bool operator==(const Tuple &lhs, const Tuple &rhs) {
-    // TODO: use epsilon for comparison?
-    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w;
-}
-
-Tuple operator+(const Tuple &lhs, const Tuple &rhs) {
-    return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w};
-}
-
-Tuple operator-(const Tuple &lhs, const Tuple &rhs) {
-    return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w};
-}
-
-Tuple operator-(const Tuple &rhs) {
-    return {-rhs.x, -rhs.y, -rhs.z, -rhs.w};
-}
-
-Tuple operator*(const Tuple &tuple, double scalar) {
-    return {tuple.x * scalar, tuple.y * scalar, tuple.z * scalar, tuple.w * scalar};
-}
-
-Tuple operator/(const Tuple &tuple, double scalar) {
-    return {tuple.x / scalar, tuple.y / scalar, tuple.z / scalar, tuple.w / scalar};
-}
-
-Tuple point(double x, double y, double z) {
-    return {x, y, z, 1.0};
-}
-
-Tuple vector(double x, double y, double z) {
-    return {x, y, z, 0.0};
-}
-
-double magnitude(const Tuple &tuple) {
-    return std::sqrt(tuple.x * tuple.x + tuple.y * tuple.y + tuple.z * tuple.z + tuple.w * tuple.w);
-}
+#include "tuple.h"
 
 TEST_CASE("a tuple with w=1 is a point", "[tuple]" ) {
     Tuple tuple{4.3, -4.2, 3.1, 1.0};
