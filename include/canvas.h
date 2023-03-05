13e4d68fe113ec0031cc7be951b96c78fc085255
Matt Clarke <matt.clarke@ess.eu>
2023-03-05 09:32:35 +0100

    basic writing of pixels to ppm

--- include/canvas.h
+++ include/canvas.h
@@ -1,7 +1,11 @@
 #ifndef RAYTRACER_CHALLENGE_CANVAS_H
 #define RAYTRACER_CHALLENGE_CANVAS_H
 
+#include <algorithm>
+#include <string>
 #include <vector>
+
+#include <fmt/core.h>
 
 #include "tuple.h"
 
@@ -22,6 +26,27 @@ void write_pixel(Canvas &c, size_t x, size_t y, Colour col) {
 
 Colour pixel_at(const Canvas &c, size_t x, size_t y) {
     return c.pixels[c.width * y + x];
+}
+
+std::string canvas_to_ppm(const Canvas &canvas) {
+    std::string result = fmt::format("P3\n{} {}\n255\n", canvas.width, canvas.height);
+    size_t i = 0;
+    for (const auto &p : canvas.pixels) {
+        int red = std::clamp(static_cast<int>(std::ceil(p.red * 255)), 0, 255);
+        int green = std::clamp(static_cast<int>(std::ceil(p.green * 255)), 0, 255);
+        int blue = std::clamp(static_cast<int>(std::ceil(p.blue * 255)), 0, 255);
+
+        result += fmt::format("{} {} {}", red, green, blue);
+        ++i;
+        if (i % canvas.width == 0) {
+            result += '\n';
+        }
+        else {
+            result += ' ';
+        }
+    }
+    std::cout << result;
+    return result;
 }
 
 #endif //RAYTRACER_CHALLENGE_CANVAS_H
--- tests/canvas_test.cpp
+++ tests/canvas_test.cpp
@@ -3,8 +3,22 @@
 
 #include <cmath>
 #include <iostream>
+#include <sstream>
+#include <string>
+
 #include "canvas.h"
 #include "tuple.h"
+
+
+std::vector<std::string> str_to_vec(const std::string &s) {
+    std::vector<std::string> result; 
+    std::istringstream iss;
+    iss.str(s);
+    for (std::string temp; std::getline(iss, temp, '\n');) {
+        result.push_back(temp);
+    }
+    return result;
+}
 
 TEST_CASE("creating a blank canvas", "[canvas]" ) {
     auto c = Canvas{10, 20};
@@ -23,4 +37,34 @@ TEST_CASE("writing pixels to a canvas", "[canvas]" ) {
     write_pixel(c, 2, 3, red);
 
     REQUIRE(pixel_at(c, 2, 3) == red);
+}
+
+TEST_CASE("constructing the PPM header", "[canvas]" ) {
+    auto c = Canvas{5, 3};
+
+    auto ppm = canvas_to_ppm(c);
+    auto lines = str_to_vec(ppm);
+
+
+    REQUIRE(lines[0] == "P3");
+    REQUIRE(lines[1] == "5 3");
+    REQUIRE(lines[2] == "255");
+}
+
+TEST_CASE("constructing the PPM pixel data", "[canvas]" ) {
+    auto canvas = Canvas{5, 3};
+    auto c1 = Colour{1.5, 0, 0};
+    auto c2 = Colour{0, 0.5, 0};
+    auto c3 = Colour{-0.5, 0, 1};
+
+    write_pixel(canvas, 0, 0, c1);
+    write_pixel(canvas, 2, 1, c2);
+    write_pixel(canvas, 4, 2, c3);
+
+    auto ppm = canvas_to_ppm(canvas);
+    auto lines = str_to_vec(ppm);
+
+    REQUIRE(lines[3] == "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+    REQUIRE(lines[4] == "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0");
+    REQUIRE(lines[5] == "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255");
 }
