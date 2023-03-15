48b56291be88a9f66064e46d5966fd77ce5d203f
Matt Clarke <matt.clarke@ess.eu>
2023-03-15 15:52:14 +0000

    basic matrices

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -17,3 +17,20 @@ TEST_CASE("constructing and inspecting a 4x4 matrix", "[matrix]" ) {
     REQUIRE(m.at(3, 0) == 13.5);
     REQUIRE(m.at(3, 2) == 15.5);
 }
+
+TEST_CASE("a 2x2 matrix ought to be representable", "[matrix]" ) {
+    auto m = Matrix{2, 2, {-3, 5, 1, -2}};
+
+    REQUIRE(m.at(0, 0) == -3);
+    REQUIRE(m.at(0, 1) == 5);
+    REQUIRE(m.at(1, 0) == 1);
+    REQUIRE(m.at(1, 1) == -2);
+}
+
+TEST_CASE("a 3x3 matrix ought to be representable", "[matrix]" ) {
+    auto m = Matrix{3, 3, {-3, 5, 0, 1, -2, -7, 0, 1, 1}};
+
+    REQUIRE(m.at(0, 0) == -3);
+    REQUIRE(m.at(1, 1) == -2);
+    REQUIRE(m.at(2, 2) == 1);
+}
