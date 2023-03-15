99220af20bd8807e52daa026567033de14316e93
Matt Clarke <matt.clarke@ess.eu>
2023-03-15 20:44:03 +0000

    formatting - not sure I like it

--- include/matrix.h
+++ include/matrix.h
@@ -34,28 +34,34 @@ Matrix operator*(const Matrix &lhs, const Matrix &rhs) {
     assert((void("matrices must be 4x4 when multiplying"), lhs.width == 4 && lhs.height == 4));
     assert((void("matrices must be 4x4 when multiplying"), rhs.width == 4 && rhs.height == 4));
 
-    std::vector<float> calculated(lhs.width * lhs.height, 0);
+    std::vector<float> calculated(16, 0);
 
-    for (size_t r = 0; r < lhs.width; ++r) {
-        for (size_t c = 0; c < lhs.height; ++c) {
-            float ans = 0;
-            for (size_t i = 0; i < lhs.width; ++i) {
-                ans += lhs.at(r, i) * rhs.at(i, c);
-            }
-            calculated[r * lhs.width + c] = ans;
+    for (size_t r = 0; r < 4; ++r) {
+        for (size_t c = 0; c < 4; ++c) {
+            calculated[r * 4 + c] = lhs.at(r, 0) * rhs.at(0, c)
+                + lhs.at(r, 1) * rhs.at(1, c)
+                + lhs.at(r, 2) * rhs.at(2, c)
+                + lhs.at(r, 3) * rhs.at(3, c);
         }
     }
-    return {lhs.width, lhs.height, calculated};
+    return {4, 4, calculated};
 }
 
 Tuple operator*(const Matrix &lhs, const Tuple &rhs) {
     assert((void("matrix must be 4x4 when multiplying against a tuple"), lhs.width == 4 && lhs.height == 4));
 
     return {
-        lhs.at(0, 0) * rhs.x + lhs.at(0, 1) * rhs.y + lhs.at(0, 2) * rhs.z + lhs.at(0, 3) * rhs.w,
-        lhs.at(1, 0) * rhs.x + lhs.at(1, 1) * rhs.y + lhs.at(1, 2) * rhs.z + lhs.at(1, 3) * rhs.w,
-        lhs.at(2, 0) * rhs.x + lhs.at(2, 1) * rhs.y + lhs.at(2, 2) * rhs.z + lhs.at(2, 3) * rhs.w,
-        lhs.at(3, 0) * rhs.x + lhs.at(3, 1) * rhs.y + lhs.at(3, 2) * rhs.z + lhs.at(3, 3) * rhs.w
+        lhs.at(0, 0) * rhs.x + lhs.at(0, 1) * rhs.y
+        + lhs.at(0, 2) * rhs.z + lhs.at(0, 3) * rhs.w,
+        
+        lhs.at(1, 0) * rhs.x + lhs.at(1, 1) * rhs.y
+        + lhs.at(1, 2) * rhs.z + lhs.at(1, 3) * rhs.w,
+        
+        lhs.at(2, 0) * rhs.x + lhs.at(2, 1) * rhs.y
+        + lhs.at(2, 2) * rhs.z + lhs.at(2, 3) * rhs.w,
+        
+        lhs.at(3, 0) * rhs.x + lhs.at(3, 1) * rhs.y
+        + lhs.at(3, 2) * rhs.z + lhs.at(3, 3) * rhs.w
     };
 }
 
