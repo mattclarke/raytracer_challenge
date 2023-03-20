ba74eddbc3db128cc7949b44fc6bce807f55d0ed
Matt Clarke <matt.clarke@ess.eu>
2023-03-20 12:18:29 +0000

    intersections and hits done

--- include/intersection.h
+++ include/intersection.h
@@ -2,6 +2,7 @@
 #define RAYTRACER_CHALLENGE_INTERSECTION_H
 
 #include <cmath>
+#include <optional>
 #include <vector>
 
 #include "sphere.h"
@@ -17,8 +18,28 @@ struct Intersection {
     {}
 };
 
-std::vector<Intersection> intersections(const Intersection &i1, const Intersection &i2) {
-    return {i1, i2};
+bool operator==(const Intersection &lhs, const Intersection &rhs) {
+    return (lhs.t == rhs.t) && (lhs.object == rhs.object);
+}
+
+std::vector<Intersection> intersections(std::initializer_list<Intersection> items) {
+    auto result = std::vector<Intersection>(items);
+
+    std::sort(result.begin(), result.end(), [](const auto &a, const auto &b)
+              {
+                  return a.t < b.t;
+              });
+
+    return result;
+}
+
+std::optional<Intersection> hit(const std::vector<Intersection> &intersections){
+    for(const auto & i : intersections) {
+        if (i.t > 0) {
+            return i;
+        }
+    }
+    return std::nullopt;
 }
 
 #endif // RAYTRACER_CHALLENGE_INTERSECTION_H
--- tests/intersection_test.cpp
+++ tests/intersection_test.cpp
@@ -12,3 +12,60 @@ TEST_CASE("an intersection encapsulates t and object", "[intersection]" ) {
     REQUIRE(i.t == 3.5);
     REQUIRE(i.object == s);
 }
+
+TEST_CASE("aggreating intersections", "[intersection]" ) {
+    auto s = Sphere{1};
+    auto i1 = Intersection{1, s};
+    auto i2 = Intersection{2, s};
+    auto xs = intersections({i2, i1});
+
+    REQUIRE(xs.size() == 2);
+    REQUIRE(xs[0].t == 1.0f);
+    REQUIRE(xs[1].t == 2.0f);
+}
+
+TEST_CASE("the hit, when all intersections have a positive t", "[intersection]" ) {
+    auto s = Sphere{1};
+    auto i1 = Intersection{1, s};
+    auto i2 = Intersection{2, s};
+    auto xs = intersections({i2, i1});
+
+    auto i = hit(xs);
+
+    REQUIRE(i == i1);
+}
+
+TEST_CASE("the hit, when some intersections have negative t", "[intersection]" ) {
+    auto s = Sphere{1};
+    auto i1 = Intersection{-1, s};
+    auto i2 = Intersection{2, s};
+    auto xs = intersections({i2, i1});
+
+    auto i = hit(xs);
+
+    REQUIRE(i == i2);
+}
+
+TEST_CASE("the hit, when all intersections have negative t", "[intersection]" ) {
+    auto s = Sphere{1};
+    auto i1 = Intersection{-1, s};
+    auto i2 = Intersection{-2, s};
+    auto xs = intersections({i2, i1});
+
+    auto i = hit(xs);
+
+    REQUIRE(!i.has_value());
+}
+
+TEST_CASE("the hit is always the lowest non-negative intersection", "[intersection]" ) {
+    auto s = Sphere{1};
+    auto i1 = Intersection{5, s};
+    auto i2 = Intersection{2, s};
+    auto i3 = Intersection{-3, s};
+    auto i4 = Intersection{2, s};
+    auto xs = intersections({i1, i2, i3, i4});
+
+    auto i = hit(xs);
+
+    REQUIRE(i == i4);
+}
