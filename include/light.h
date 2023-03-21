6ec306aac24ed60a2ad750d8d3d1d0142ea5f678
Matt Clarke <matt.clarke@ess.eu>
2023-03-21 16:27:40 +0000

    lighting done

--- include/light.h
+++ include/light.h
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <vector>
 
+#include "material.h"
 #include "tuple.h"
 #include "utils.h"
 
@@ -15,5 +16,26 @@ struct PointLight {
         position(position), intensity(intensity) {
     }
 };
+
+Colour lighting(const Material &material, const PointLight &light, const Tuple &point, const Tuple &eyev, const Tuple &normalv) {
+    auto effective_colour = material.colour * light.intensity;auto lightv = normalise(light.position - point);
+    auto ambient = effective_colour * material.ambient;
+    auto light_dot_normal = dot(lightv, normalv);
+    auto diffuse = Colour{0, 0, 0};
+    auto specular = Colour{0, 0, 0};
+
+    if (light_dot_normal >= 0) {
+        diffuse = effective_colour * material.diffuse * light_dot_normal;
+
+        auto reflectv = reflect(-lightv, normalv);
+        auto reflect_dot_eye = dot(reflectv, eyev);
+
+        if (reflect_dot_eye > 0) {
+            auto factor = std::pow(reflect_dot_eye, material.shininess);
+            specular = light.intensity * material.specular * factor;
+        }
+    }
+    return ambient + diffuse + specular;
+}
 
 #endif //RAYTRACER_CHALLENGE_LIGHT_H
--- tests/material_test.cpp
+++ tests/material_test.cpp
@@ -1,7 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include "sphere.h"
+#include "light.h"
+#include "material.h"
 #include "tuple.h"
 
 TEST_CASE("the default material", "[material]" ) {
@@ -12,4 +13,69 @@ TEST_CASE("the default material", "[material]" ) {
     REQUIRE(m.diffuse == 0.9f);
     REQUIRE(m.specular == 0.9f);
     REQUIRE(m.shininess == 200.0f);
+}
+
+TEST_CASE("lighting with the eye between the light and the surface", "[material]" ) {
+    auto m = Material{};
+    auto position = point(0, 0, 0);
+
+    auto eyev = vector(0, 0, -1);
+    auto normalv = vector(0, 0, -1);
+    auto light = PointLight{point(0, 0, -10), Colour{1, 1, 1}};
+
+    auto result = lighting(m, light, position, eyev, normalv);
+
+    REQUIRE(result == Colour{1.9, 1.9, 1.9});
+}
+
+TEST_CASE("lighting with the eye between the light and the surface, eye offset 45 degrees", "[material]" ) {
+    auto m = Material{};
+    auto position = point(0, 0, 0);
+
+    auto eyev = vector(0, std::sqrt(2) / 2, -std::sqrt(2) / 2);
+    auto normalv = vector(0, 0, -1);
+    auto light = PointLight{point(0, 0, -10), Colour{1, 1, 1}};
+
+    auto result = lighting(m, light, position, eyev, normalv);
+
+    REQUIRE(result == Colour{1.0, 1.0, 1.0});
+}
+
+TEST_CASE("lighting with eye opposite surface, light offset 45 degrees", "[material]" ) {
+    auto m = Material{};
+    auto position = point(0, 0, 0);
+
+    auto eyev = vector(0, 0, -1);
+    auto normalv = vector(0, 0, -1);
+    auto light = PointLight{point(0, 10, -10), Colour{1, 1, 1}};
+
+    auto result = lighting(m, light, position, eyev, normalv);
+
+    REQUIRE(result == Colour{0.7364, 0.7364, 0.7364});
+}
+
+TEST_CASE("lighting with the eye in the path of the reflection vector", "[material]" ) {
+    auto m = Material{};
+    auto position = point(0, 0, 0);
+
+    auto eyev = vector(0, -std::sqrt(2) / 2, -std::sqrt(2) / 2);
+    auto normalv = vector(0, 0, -1);
+    auto light = PointLight{point(0, 10, -10), Colour{1, 1, 1}};
+
+    auto result = lighting(m, light, position, eyev, normalv);
+
+    REQUIRE(result == Colour{1.6364, 1.6364, 1.6364});
+}
+
+TEST_CASE("lighting with the light behind the surface", "[material]" ) {
+    auto m = Material{};
+    auto position = point(0, 0, 0);
+
+    auto eyev = vector(0, 0, -1);
+    auto normalv = vector(0, 0, -1);
+    auto light = PointLight{point(0, 0, 10), Colour{1, 1, 1}};
+
+    auto result = lighting(m, light, position, eyev, normalv);
+
+    REQUIRE(result == Colour{0.1, 0.1, 0.1});
 }
