f3b3fd83170244e468b2680a178d4d4c9ff4d0f8
Matt Clarke <matt.clarke@ess.eu>
2024-11-03 12:25:45 +0100

    canvas to ppm done

--- src/canvas.rs
+++ src/canvas.rs
@@ -8,11 +8,11 @@ struct Canvas {
     pixels: Vec<Color>,
 }
 
-fn normalise_color_value(color: &Color) -> (i32, i32, i32) {
+fn normalise_color_value(color: &Color) -> [i32; 3] {
     let red = (color.red * 255.0).round() as i32;
     let green = (color.green * 255.0).round() as i32;
     let blue = (color.blue * 255.0).round() as i32;
-    (red.clamp(0, 255), green.clamp(0, 255), blue.clamp(0, 255))
+    [red.clamp(0, 255), green.clamp(0, 255), blue.clamp(0, 255)]
 }
 
 impl Canvas {
@@ -35,16 +35,37 @@ impl Canvas {
     }
 
     pub fn to_ppm(&self) -> String {
+        // PPM format:
+        // first line is version: always "P3"
+        // second line is "width space height"
+        // third line is the max colour value: 255
+        // Next lines are the pixels in RGB order
+        //
+        // PPM line limit is 70 chars including the newline, so needs to be wrapped
         let mut result = format!("P3\n{} {}\n255\n", self.width, self.height);
         for y in 0..self.height {
             let mut line = String::new();
+            let mut length = 0;
             for x in 0..self.width {
                 let index = y * self.width + x;
                 let pixel = &self.pixels[index];
                 let values = normalise_color_value(pixel);
-                line.push_str(&format!("{} {} {} ", values.0, values.1, values.2));
+                if line.len() == 0 {
+                    line.push_str(&format!("{} {} {}", values[0], values[1], values[2]));
+                    length = line.len();
+                } else {
+                    for v in values.iter() {
+                        let sv = format!(" {}", v);
+                        if length + sv.len() >= 69 {
+                            line.push_str("\n");
+                            length = 0;
+                        }
+                        line.push_str(&sv);
+                        length += sv.len();
+                    }
+                }
             }
-            result.push_str(&line.trim_end());
+            result.push_str(&line);
             result.push_str("\n");
         }
         result
@@ -95,5 +116,40 @@ mod tests {
         assert_eq!(lines[3], "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
         assert_eq!(lines[4], "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0");
         assert_eq!(lines[5], "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255");
+    }
+
+    #[test]
+    fn splitting_long_lines_in_ppm_files() {
+        let mut c = Canvas::new(10, 2);
+        for y in 0..2 {
+            for x in 0..10 {
+                c.write_pixel(x, y, Color::new(1.0, 0.8, 0.6));
+            }
+        }
+        let ppm = c.to_ppm();
+        let lines = ppm.split("\n").collect::<Vec<_>>();
+        assert_eq!(
+            lines[3],
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+        );
+        assert_eq!(
+            lines[4],
+            " 153 255 204 153 255 204 153 255 204 153 255 204 153"
+        );
+        assert_eq!(
+            lines[5],
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+        );
+        assert_eq!(
+            lines[6],
+            " 153 255 204 153 255 204 153 255 204 153 255 204 153"
+        );
+    }
+
+    #[test]
+    fn ppm_files_terminated_by_newline() {
+        let c = Canvas::new(5, 3);
+        let ppm = c.to_ppm();
+        assert_eq!(ppm.chars().last().unwrap(), '\n');
     }
 }
