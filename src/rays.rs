5f3abce91f7d9d0005e99eb7278e9394ecb4f106
Matt Clarke <matto.kuraku@gmail.com>
2025-08-30 13:31:03 +0200

    calculate point from ray

--- src/rays.rs
+++ src/rays.rs
@@ -9,6 +9,15 @@ fn ray(origin: Tuple, direction: Tuple) -> Ray {
     Ray { origin, direction }
 }
 
+fn position(ray: &Ray, t: f32) -> Tuple {
+    Tuple {
+        x: ray.origin.x + ray.direction.x * t,
+        y: ray.origin.y + ray.direction.y * t,
+        z: ray.origin.z + ray.direction.z * t,
+        w: ray.origin.w + ray.direction.w * t,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::tuple::{point, vector};
@@ -22,5 +31,14 @@ mod tests {
         let ray = ray(origin.clone(), direction.clone());
         assert_eq!(ray.origin, origin);
         assert_eq!(ray.direction, direction);
+    }
+
+    #[test]
+    fn create_a_point_from_a_distance() {
+        let ray = ray(point(2.0, 3.0, 4.0), vector(1.0, 0.0, 0.0));
+        assert_eq!(position(&ray, 0.0), point(2.0, 3.0, 4.0));
+        assert_eq!(position(&ray, 1.0), point(3.0, 3.0, 4.0));
+        assert_eq!(position(&ray, -1.0), point(1.0, 3.0, 4.0));
+        assert_eq!(position(&ray, 2.5), point(4.5, 3.0, 4.0));
     }
 }
