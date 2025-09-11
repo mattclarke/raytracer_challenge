bc670f4fa543c2fd30600fd06366d24f3c85ade7
Matt Clarke <matto.kuraku@gmail.com>
2025-09-11 16:13:33 +0200

    calculate normal for translated sphere

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,5 +1,5 @@
 use crate::{
-    matrix::Matrix,
+    matrix::{inverse, Matrix},
     tuple::{normalise, point, Tuple},
 };
 
@@ -21,15 +21,21 @@ pub fn sphere() -> Sphere {
 }
 
 fn normal_at(s: &Sphere, p: &Tuple) -> Tuple {
-    normalise(&(p - &point(0.0, 0.0, 0.0)))
+    let obj_point = inverse(&s.transform) * p;
+    let obj_normal = obj_point - point(0.0, 0.0, 0.0);
+    let mut world_normal = inverse(&s.transform).transpose() * obj_normal;
+    world_normal.w = 0.0;
+    normalise(&world_normal)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::f32::consts::PI;
+
     use crate::{
         matrix::Matrix,
         rays::{intersect, ray},
-        transformations::{scaling, translation},
+        transformations::{rotation_z, scaling, translation},
         tuple::{normalise, point, vector},
     };
 
@@ -127,5 +133,22 @@ mod tests {
         let v = 3.0_f32.sqrt() / 3.0;
         let n = normal_at(&s, &point(v, v, v));
         assert_eq!(n, normalise(&n));
+    }
+
+    #[test]
+    fn normal_on_translated_sphere() {
+        let mut s = sphere();
+        s.transform = translation(0.0, 1.0, 0.0);
+        let n = normal_at(&s, &point(0.0, 1.70711, -0.70711));
+        assert_eq!(n, vector(0.0, 0.70711, -0.70711));
+    }
+
+    #[test]
+    fn normal_on_transformed_sphere() {
+        let mut s = sphere();
+        let m = scaling(1.0, 0.5, 1.0) * rotation_z(PI / 5.0);
+        s.transform = m;
+        let n = normal_at(&s, &point(0.0, 2.0_f32.sqrt() / 2.0, -2.0_f32.sqrt() / 2.0));
+        assert_eq!(n, vector(0.0, 0.97014, -0.24254));
     }
 }
